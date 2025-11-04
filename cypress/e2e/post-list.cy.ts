30d30d345fc72449a2b73e7763ae4b338f5d3beb
baeksangsu-agricore <baeksangsu@it.farmfam.co.kr>
2025-11-04 14:32:21 +0900

    fix

--- cypress/e2e/post-list.cy.ts
+++ cypress/e2e/post-list.cy.ts
@@ -14,7 +14,7 @@ describe('Post list page tests', () => {
 
     it('should navigate to post detail page on post click', () => {
         cy.visit('http://localhost:3000/posts');
-        
+
         const firstPost = mockPosts[0];
 
         cy.get(`#post-article-${firstPost.id}`).click();
