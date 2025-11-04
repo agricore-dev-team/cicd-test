9cb79f24d27318166c6f14411ad95cb1cc11064f
baeksangsu-agricore <baeksangsu@it.farmfam.co.kr>
2025-11-04 15:16:17 +0900

    test

--- cypress/e2e/post-list.cy.ts
+++ cypress/e2e/post-list.cy.ts
@@ -12,7 +12,6 @@ describe('Post list page tests', () => {
         });
     });
 
-    
     it('should navigate to post detail page on post click', () => {
         cy.visit('http://localhost:3000/posts');
 
